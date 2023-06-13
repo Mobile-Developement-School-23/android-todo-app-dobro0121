0c650ee22d15c931fdf0575874c48c3d0c8037cf
Maria <ivanova.m.a2@edu.mirea.ru>
2023-06-13 23:57:12 +0300

    Add strings of Spinner, make transaction between 2 fragments with data (need some fix in future)

--- app/src/main/java/com/example/todoapp/AddTaskFragment.kt
+++ app/src/main/java/com/example/todoapp/AddTaskFragment.kt
@@ -7,11 +7,16 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.navigation.Navigation
 
 
 class AddTaskFragment : Fragment() {
+
+    var textTask: String? = null
+    var textImportant: String? = null
+    var textDoneTo: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,22 +26,37 @@ class AddTaskFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_task, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val buttonClose: ImageButton = view.findViewById(R.id.imageButtonClose)
-        val TextViewSafe: TextView = view.findViewById(R.id.SafeTextView)
+        val textViewDelete: TextView = view.findViewById(R.id.DeleteTextView)
+        val textViewSafe: TextView = view.findViewById(R.id.SafeTextView)
         val editViewTask: EditText = view.findViewById(R.id.EditTextTaskOf)
-        TextViewSafe.setOnClickListener(){
+
+        textTask = arguments?.getString("textOfTask")
+        textImportant = arguments?.getString("importance")
+        //textDoneTo = arguments?.getString("deadline")
+        editViewTask.setText(textTask)
+        val spinner: Spinner = view.findViewById(R.id.spinnerImportant)
+        //?? подумать как передавать важность
+        val dateTextView: TextView = view.findViewById(R.id.dateTextView)
+        dateTextView.text = textDoneTo
+
+        textViewSafe.setOnClickListener(){
             if(editViewTask != null){
                 // нужно сохранить данные все
             }
+            Navigation.findNavController(view).navigate(R.id.action_addTaskFragment_to_mainFragment)
         }
 
         buttonClose.setOnClickListener(){
+            Navigation.findNavController(view).navigate(R.id.action_addTaskFragment_to_mainFragment)
+        }
+
+        textViewDelete.setOnClickListener() {
             Navigation.findNavController(view).navigate(R.id.action_addTaskFragment_to_mainFragment)
         }
     }
